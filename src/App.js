ce2c29be6d1eed0af26acc0781a9e2af67bc1403
elenapuzic <61571552+elenapuzic@users.noreply.github.com>
2020-08-06 18:49:41 +0300

    1.6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <>
+              <h1>give feedback</h1>
+              </>
+        <div>
+            <button onClick={() => setGood(good + 1)}>
+                good
+            </button>
+
+            <button onClick={() => setNeutral(neutral + 1)}>
+            neutral
+            </button>
+
+            <button onClick={() => setBad(bad + 1)}>
+                bad
+            </button>
+        </div>
+          <>
+              <h1>statistics</h1>
+              <ul>
+               <li>good {good}</li>
+                  <li>neutral {neutral}</li>
+                  <li>bad {bad}</li>
+              </ul>
+              </>
+      </div>
+  )
 }
 
 export default App;
