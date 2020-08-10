eca8efed3fb3e0c338a79fa4c10b39c479349cb4
elenapuzic <61571552+elenapuzic@users.noreply.github.com>
2020-08-10 15:49:46 +0300

    1.7

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
 import React, {useState} from 'react';
 
-const App = () => {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    const App = () => {
+        const [good, setGood] = useState(0)
+        const [neutral, setNeutral] = useState(0)
+        const [bad, setBad] = useState(0)
 
-  return (
-      <div>
-          <>
-              <h1>give feedback</h1>
-              </>
-        <div>
-            <button onClick={() => setGood(good + 1)}>
-                good
-            </button>
+        return (
+            <div>
+                <h1>give feedback</h1>
+                <div>
+                    <button onClick={() => setGood(good + 1)}>good</button>
+                    <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+                    <button onClick={() => setBad(bad + 1)}>bad</button>
+                </div>
+                <h1>statistics</h1>
+                <ul>
+                    <li>good {good}</li>
+                    <li>neutral {neutral}</li>
+                    <li>bad {bad}</li>
+                </ul>
+                <div>
+                    <p>all {good + neutral + bad}</p>
+                    <p>average {(good + neutral + bad) / 3}</p>
+                    <p>positive {((good / (good + neutral + bad)) * 100) || 0} %</p>
+                </div>
+            </div>
+        )
+    }
 
-            <button onClick={() => setNeutral(neutral + 1)}>
-            neutral
-            </button>
-
-            <button onClick={() => setBad(bad + 1)}>
-                bad
-            </button>
-        </div>
-          <>
-              <h1>statistics</h1>
-              <ul>
-               <li>good {good}</li>
-                  <li>neutral {neutral}</li>
-                  <li>bad {bad}</li>
-              </ul>
-              </>
-      </div>
-  )
-}
-
-export default App;
+    export default App;
